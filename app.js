9ed75f4be9efaac455ec9f262733608046b9c6fc
Bor K Nelson <bornelsonk@gmail.com>
2025-07-12 12:45:22 +0300

    Add logic to get computer choice
    The computer choice is generated form random numbers between 0 and 10 using math.Random function

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function getComputerChoice(){
+    let number=Math.random()*10;
+    let result=0;
+    if (number>0 && number<=3){
+        result="Paper";
+    }
+    else if(number>3 && number<6){
+        result="Scissor";
+    }
+    else{
+        result="Rock";
+    }
+}
